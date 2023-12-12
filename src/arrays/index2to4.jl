3f0218d840c9c3df284401abcff4aacaf73abb93
adfeel220 <adfeel220@gmail.com>
2023-12-12 14:41:26 +0100

    Rename optimized 2-4 index version of DDA

--- src/arrays/index2to4.jl
+++ src/arrays/index2to4.jl
@@ -1,65 +1,6 @@
 
 """
-    DimensionFreeData{N,T}
-A data of type `T` labeled by index as an `NTuple{N, Int}`.
-Overloads `isless` and `==` for that the comparison is only the index.
-Overloads `sorted_rank` for `AVLTree{DimensionFreeData}` to use `Tuple` or `Vararg` to index `DimensionFreeData`
-"""
-mutable struct DimensionFreeData{T,N}
-    data::T
-    index::NTuple{N,Int}
-end
-function DimensionFreeData{T}(index::NTuple{N,Int}) where {N,T}
-    return DimensionFreeData{T,N}(zero(T), index)
-end
-function Base.show(io::IO, data::DimensionFreeData)
-    return show(io, "DimensionFreeData: Value $(data.data) indexed by $(data.index)")
-end
-Base.isless(d1::DimensionFreeData, d2::DimensionFreeData) = d1.index < d2.index
-Base.:(==)(d1::DimensionFreeData, d2::DimensionFreeData) = d1.index == d2.index
-Base.:(==)(d::DimensionFreeData, idx::Vararg) = d.index == idx
-
-"""
-    find_node(tree, index)
-Find node on an AVLTree storing DimensionFreeData with given index
-"""
-function find_node(tree::AVLTree{DimensionFreeData{T,N}}, index::NTuple{N,Int}) where {T,N}
-    prev = nothing
-    node = tree.root
-    while !isnothing(node) && node.data.index != index
-        prev = node
-        if index < node.data.index
-            node = node.leftChild
-        else
-            node = node.rightChild
-        end
-    end
-
-    if !isnothing(node)
-        return node
-    end
-    return nothing
-end
-function find_data(tree::AVLTree{DimensionFreeData{T,N}}, index::NTuple{N,Int}) where {T,N}
-    node = find_node(tree, index)
-    if isnothing(node)
-        return nothing
-    end
-    return node.data.data
-end
-function set_data!(
-    tree::AVLTree{DimensionFreeData{T,N}}, value::T, index::NTuple{N,Int}
-) where {N,T}
-    node = find_node(tree, index)
-    if isnothing(node)
-        insert!(tree, DimensionFreeData{T,N}(value, index))
-    else
-        node.data.data = value
-    end
-end
-
-"""
-    DynamicDimensionArray{T}
+    DynamicDimensionArray2to4{T}
 A dimension free array in which overindexing is allowed. No out of bound error for this array.
 An special optimized version that only allows index to have length 2-4, as generic implementation generates
 instable type that slows down computation.
@@ -71,7 +12,7 @@ The indexing of this array follows the rules below:
 while `arr[1,2,3]` doesn't exist but `arr[2,3]` exists, return `arr[2,3]`
 3. If non of the degenerated indices exists, return the default value.
 """
-mutable struct DynamicDimensionArray{T}
+mutable struct DynamicDimensionArray2to4{T}
     d2::AVLTree{DimensionFreeData{T,2}}
     d3::AVLTree{DimensionFreeData{T,3}}
     d4::AVLTree{DimensionFreeData{T,4}}
@@ -79,24 +20,24 @@ mutable struct DynamicDimensionArray{T}
     default::T
 end
 
-Base.length(arr::DynamicDimensionArray) = length(arr.d2) + length(arr.d3) + length(arr.d4)
+Base.length(arr::DynamicDimensionArray2to4) = length(arr.d2) + length(arr.d3) + length(arr.d4)
 
-function Base.show(io::IO, arr::DynamicDimensionArray)
+function Base.show(io::IO, arr::DynamicDimensionArray2to4)
     return show(
         io,
-        """DynamicDimensionArray{$(typeof(arr.default))}: $(length(arr)) registered entries with default = $(arr.default).""",
+        """DynamicDimensionArray2to4{$(typeof(arr.default))}: $(length(arr)) registered entries with default = $(arr.default).""",
     )
 end
 
 """
-    DynamicDimensionArray([default=0.0])
-Create an empty `DynamicDimensionArray` with a default value (`{Float64}(0.0)` if not specified).
+    DynamicDimensionArray2to4([default=0.0])
+Create an empty `DynamicDimensionArray2to4` with a default value (`{Float64}(0.0)` if not specified).
 """
-function DynamicDimensionArray(default::T=zero(Float64)) where {T}
+function DynamicDimensionArray2to4(default::T=zero(Float64)) where {T}
     t2 = AVLTree{DimensionFreeData{T,2}}()
     t3 = AVLTree{DimensionFreeData{T,3}}()
     t4 = AVLTree{DimensionFreeData{T,4}}()
-    return DynamicDimensionArray{T}(t2, t3, t4, default)
+    return DynamicDimensionArray2to4{T}(t2, t3, t4, default)
 end
 
 """
@@ -107,7 +48,7 @@ function degenerate_tuple(t::NTuple{N,T})::NTuple{N - 1,T} where {N,T}
     return @inbounds ntuple(i -> t[i + 1], N - 1)
 end
 
-function Base.getindex(arr::DynamicDimensionArray{T}, index::Vararg{Int}) where {T}
+function Base.getindex(arr::DynamicDimensionArray2to4{T}, index::Vararg{Int}) where {T}
     if length(index) > 4
         index = index[(end - 3):end]
     end
@@ -134,7 +75,7 @@ function Base.getindex(arr::DynamicDimensionArray{T}, index::Vararg{Int}) where 
     return arr.default
 end
 function Base.setindex!(
-    arr::DynamicDimensionArray{T}, value::T, index::Vararg{Int}
+    arr::DynamicDimensionArray2to4{T}, value::T, index::Vararg{Int}
 ) where {T}
     if length(index) == 4
         set_data!(arr.d4, value, index)
@@ -146,7 +87,7 @@ function Base.setindex!(
     return arr
 end
 
-function Base.iterate(arr::DynamicDimensionArray{T}, i=1) where {T}
+function Base.iterate(arr::DynamicDimensionArray2to4{T}, i=1) where {T}
     if i > length(arr)
         return nothing
     end
@@ -164,7 +105,7 @@ function Base.iterate(arr::DynamicDimensionArray{T}, i=1) where {T}
     end
 end
 
-function delete!(arr::DynamicDimensionArray{T}, index::NTuple{N,Int}) where {T,N}
+function delete!(arr::DynamicDimensionArray2to4{T}, index::NTuple{N,Int}) where {T,N}
     if length(index) == 4
         delete!(arr.d4, DimensionFreeData{T}(index))
     elseif length(index) == 3
