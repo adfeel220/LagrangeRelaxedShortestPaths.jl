3e817ca5658ada72bffaaa6e2fc0c4cd3967f28b
adfeel220 <adfeel220@gmail.com>
2023-12-22 15:10:23 +0100

    Add optional choices for users

--- src/multiplier.jl
+++ src/multiplier.jl
@@ -91,6 +91,58 @@ function compute_gradient(
 end
 
 """
+    compute_nonnegative_gradient(
+        vertex_multiplier, edge_multiplier, vertex_occupancy, edge_occupancy, num_agents;
+        perturbation, rng
+    )
+Compute nonnegative gradient given the current occupancy table of agents on the network.
+Return gradients with respect to vertex and edge conflicts.
+This may lead to faster search to a feasible solution but suboptimal.
+
+# Arguments
+- `vertex_multiplier::AbstractDynamicDimensionArray{C}`: Lagrange multiplier for vertex conflicts
+with type of cost `C`, indexed by (time, vertex)
+- `edge_multiplier::AbstractDynamicDimensionArray{C}`: Lagrange multiplier for edge conflicts
+with type of cost `C`, indexed by (time, vertex)
+- `vertex_occupancy::VertexConflicts{T,V,A}`: occupancy table of vertices with type of time `T`,
+vertex `V`, agent `A`
+- `edge_occupancy::EdgeConflicts{T,V,A}`: occupancy table of edges with type of time `T`,
+vertex `V`, agent `A`
+- `num_agents::Int`: Number of agents in the network, for normalizing gradient size
+
+# Keyword arguments
+- `perturbation`: ratio of perturbation (± percentage), by default `0.0`
+- `rng`: random generator, by default `default_rng()`
+"""
+function compute_nonnegative_gradient(
+    vertex_multiplier::AbstractDynamicDimensionArray{C},
+    edge_multiplier::AbstractDynamicDimensionArray{C},
+    vertex_occupancy::VertexConflicts{T,V,A},
+    edge_occupancy::EdgeConflicts{T,V,A},
+    num_agents::Int;
+    perturbation=0.0,
+    rng=default_rng(),
+) where {C,T,V,A}
+    vertex_grad = empty(vertex_multiplier; default=zero(C))
+    edge_grad = empty(edge_multiplier; default=zero(C))
+
+    # vertex conflicts
+    for ((timestamp, vertex), agents) in vertex_occupancy
+        # Multiple agents occupies this vertex, update 
+        violation = (length(agents) - one(C)) / (num_agents - one(C))
+        vertex_grad[timestamp, vertex] = rand_perturbation(violation, perturbation; rng)
+    end
+
+    # edge conflicts
+    for ((timestamp, from_v, to_v), agents) in edge_occupancy
+        violation = (length(agents) - one(C)) / (num_agents - one(C))
+        edge_grad[timestamp, from_v, to_v] = rand_perturbation(violation, perturbation; rng)
+    end
+
+    return vertex_grad, edge_grad
+end
+
+"""
     update_multiplier!(
         vertex_multiplier, edge_multiplier, vertex_optimizer, edge_optimizer,
         vertex_occupancy, edge_occupancy; perturbation, rng
@@ -126,6 +178,56 @@ function update_multiplier!(
     rng=default_rng(),
 ) where {C,T,V,A}
     vertex_grad, edge_grad = compute_gradient(
+        vertex_multiplier,
+        edge_multiplier,
+        vertex_occupancy,
+        edge_occupancy,
+        num_agents;
+        perturbation,
+        rng,
+    )
+    step!(vertex_multiplier, vertex_optimizer, vertex_grad)
+    step!(edge_multiplier, edge_optimizer, edge_grad)
+
+    return vertex_grad, edge_grad
+end
+
+"""
+    update_nondecreasing_multiplier!(
+        vertex_multiplier, edge_multiplier, vertex_optimizer, edge_optimizer,
+        vertex_occupancy, edge_occupancy; perturbation, rng
+    )
+Update the Lagrange multipleir based on the current conflicts table
+
+# Arguments
+- `vertex_multiplier::AbstractDynamicDimensionArray{C}`: Lagrange multiplier for vertex conflicts
+with type of cost `C`, indexed by (time, vertex)
+- `edge_multiplier::AbstractDynamicDimensionArray{C}`: Lagrange multiplier for edge conflicts
+with type of cost `C`, indexed by (time, vertex)
+- `vertex_optimizer::Optimizer{C}`: optimizer for gradient ascend on the vertex multiplier
+- `edge_optimizer::Optimizer{C}`: optimizer for gradient ascend on the edge multiplier
+- `vertex_occupancy::VertexConflicts{T,V,A}`: occupancy table of vertices with type of time `T`,
+vertex `V`, agent `A`
+- `edge_occupancy::EdgeConflicts{T,V,A}`: occupancy table of edges with type of time `T`,
+vertex `V`, agent `A`
+- `num_agents::Int`: Number of agents in the network, for normalizing gradient size
+
+# Keyword arguments
+- `perturbation`: ratio of perturbation (± percentage), by default `0.0`
+- `rng`: random generator, by default `default_rng()`
+"""
+function update_nondecreasing_multiplier!(
+    vertex_multiplier::AbstractDynamicDimensionArray{C},
+    edge_multiplier::AbstractDynamicDimensionArray{C},
+    vertex_optimizer::AbstractOptimizer{C},
+    edge_optimizer::AbstractOptimizer{C},
+    vertex_occupancy::VertexConflicts{T,V,A},
+    edge_occupancy::EdgeConflicts{T,V,A},
+    num_agents::Int;
+    perturbation=0.0,
+    rng=default_rng(),
+) where {C,T,V,A}
+    vertex_grad, edge_grad = compute_nonnegative_gradient(
         vertex_multiplier,
         edge_multiplier,
         vertex_occupancy,
